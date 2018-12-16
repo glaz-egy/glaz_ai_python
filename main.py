d2cf891e93723de15e70873e69bd1f6477690a22
Glaz Egy <rei.izayoi.0804@gmail.com>
2018-12-16 23:09:12 +0900

    更新

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import MeCab
 import twitterbot
 from random import choice
+import datetime
+import time
 
-def DataCreate(text, mecab):
+def DataCreate(text, mecab, num=3):
     text = text.strip()
     if text == '\n': return
     raw = mecab.parse(text)
@@ -12,40 +14,51 @@ def DataCreate(text, mecab):
         datalist.append(data.split('\t')[0])
     datalist.remove('EOS')
     returnlist = []
-    for i in range(len(datalist)):
-        returnlist.append(['BOS' if i == 0 else datalist[i-1], datalist[i], datalist[i+1] if i+1 < len(datalist) else 'EOS'])
+    for i in range(len(datalist)+ (0 if num == 3 else 1)):
+        if num == 2:
+            returnlist.append(['BOS' if i == 0 else datalist[i-1], datalist[i] if i < len(datalist) else 'EOS'])
+        elif num == 3:
+            returnlist.append(['BOS' if i == 0 else datalist[i-1], datalist[i], datalist[i+1] if i+1 < len(datalist) else 'EOS'])
     return returnlist
 
-def Markov(datalist):
-    nextstring = next(datalist, ['BOS'], Start=True)
-    out = nextstring[0]
+def Markov(datalist, num=3):
+    nextstring = next(datalist, ['BOS'], Start=True, num=num)
+    out = nextstring[0] if nextstring[0] != 'BOS' else ''
     while True:
         if nextstring[1] == 'EOS': return out
         out += nextstring[1]
-        nextstring = next(datalist, nextstring)
+        nextstring = next(datalist, nextstring, num=num)
         if len(out) > 140:
-            nextstring = next(datalist, ['BOS'], Start=True)
-            out = nextstring[0] + nextstring[1]
+            nextstring = next(datalist, ['BOS'], Start=True, num=num)
+            out = nextstring[0] if nextstring[0] != 'BOS' else ''
 
-def next(datalist, s, Start=False):
-    if Start: nextlist = [data for data in datalist if data[0] == s[0]]
-    else: nextlist = [data for data in datalist if data[0] == s[0] and data[1] == s[1]]
+def next(datalist, s, Start=False, num=3):
+    if num == 2:
+        if Start: nextlist = [data for data in datalist if data[0] == s[0]]
+        else: nextlist = [data for data in datalist if data[0] == s[1]]
+    else: nextlist = [data for data in datalist if data[0] == s[0] and (Start or data[1] == s[1])]
     ch = choice(nextlist)
     print(ch)
-    return [ch[1], ch[2]]
+    return (ch if num == 2 else ch[1:])
 
 def main():
-    twitterbot.get_tweet()
+    #twitterbot.get_tweet()
+    num=2
     with open('textdata.txt', 'r', encoding='utf-8') as f:
         textdata = f.readlines()
     datalist = []
     mecab = MeCab.Tagger()
     for text in textdata:
-        datalist.extend(DataCreate(text, mecab))
-    tweettext = Markov(datalist)
+        datalist.extend(DataCreate(text, mecab, num=num))
+    tweettext = Markov(datalist, num=num)
     print(tweettext)
     twitterbot.post_tweet(tweettext)
+    twitterbot.auto_follow()
+    twitterbot.check_limit()
 
 if __name__=='__main__':
-    main()
+    while True:
+        print(datetime.datetime.now().strftime('[%Y/%m/%d %H:%M:%S]'))
+        main()
+        time.sleep(600)
     
--- twitterbot.py
+++ twitterbot.py
@@ -11,33 +11,63 @@ api = twitter.Api(consumer_key=config['OAuth']['consumer_key'],\
                 access_token_key=config['OAuth']['access_token_key'],\
                 access_token_secret=config['OAuth']['access_token_secret'])
 
+def auto_follow():
+    try:
+        Followers = api.GetFollowers()
+        Friends = api.GetFriends()
+        for follower in Followers:
+            if not follower in Friends:
+                api.CreateFriendship(user_id=follower.id)
+    except twitter.error.TwitterError as err:
+        print(err)
+
+def check_limit():
+    url = '%s/statuses/user_timeline.json' % (api.base_url)
+    print(api.CheckRateLimit(url))
+    url = '%s/statuses/update.json' % (api.base_url)
+    print(api.CheckRateLimit(url))
+    url = '%s/1.1/followers/list.json' % (api.base_url)
+    print(api.CheckRateLimit(url))
+    url = '%s/1.1/friends/list.json' % (api.base_url)
+    print(api.CheckRateLimit(url))
+
 def get_tweet(count=200):
-    statuse = api.GetUserTimeline(config['User']['user_id'], count=count)
-    if not os.path.isfile('textdata.txt'):
-        with open('textdata.txt', 'w') as f:
-            pass
-    with open('textdata.txt', 'r', encoding='utf-8') as f:
-        ExistData = f.readlines()
-    for s in statuse:
-        MucthText = re.search(r"@.*\s", s.text)
-        if MucthText is None: texts = s.text
-        else:
-            texts = MucthText.string.replace(" ", "")
-            texts = texts.replace("@", "")
-        MucthText = re.search(r"http.*", texts)
-        texts = texts if MucthText is None else texts.replace(MucthText.group(0), '')
-        MucthText = re.search(r"#.*", texts)
-        texts = texts if MucthText is None else texts.replace(MucthText.group(0), '')
-        if not "RT" in texts and None == s.retweeted_status and re.match(r".*@.*(より|さんから)", texts) is None:
-            texts = texts.strip().split('\n')
-            for text in texts:
-                if not text == '\n' and not text == '' and re.match(r"\s+", text) is None and not text+'\n' in ExistData:
-                    ExistData.append(text)
-                    with open('textdata.txt', 'a', encoding='utf-8') as f:
-                        f.write(text+'\n')
+    try:
+        if '@' in config['User']['user_id']: UserId = config['User']['user_id'].split('@')
+        else: UserId = [config['User']['user_id']]
+        statuse = []
+        for user in UserId:
+            statuse.extend(api.GetUserTimeline(user.strip(), count=count))
+        if not os.path.isfile('textdata.txt'):
+            with open('textdata.txt', 'w') as f:
+                pass
+        with open('textdata.txt', 'r', encoding='utf-8') as f:
+            ExistData = f.readlines()
+        for s in statuse:
+            MucthText = re.search(r"@.*\s", s.text)
+            if MucthText is None: texts = s.text
+            else:
+                texts = MucthText.string.replace(" ", "")
+                texts = texts.replace("@", "")
+            MucthText = re.search(r"http.*", texts)
+            texts = texts if MucthText is None else texts.replace(MucthText.group(0), '')
+            MucthText = re.search(r"#.*", texts)
+            texts = texts if MucthText is None else texts.replace(MucthText.group(0), '')
+            if not "RT" in texts and None == s.retweeted_status and re.match(r".*@.*(より|さんから)", texts) is None:
+                texts = texts.strip().split('\n')
+                for text in texts:
+                    if not text == '\n' and not text == '' and re.match(r"\s+", text) is None and not text+'\n' in ExistData:
+                        ExistData.append(text)
+                        with open('textdata.txt', 'a', encoding='utf-8') as f:
+                            f.write(text+'\n')
+    except twitter.error.TwitterError as err:
+        print(err)
 
 def post_tweet(tweettext):
-    api.PostUpdates(tweettext)
+    try:
+        api.PostUpdates(tweettext)
+    except twitter.error.TwitterError as err:
+        print(err)
 
 if __name__=='__main__':
     pass
